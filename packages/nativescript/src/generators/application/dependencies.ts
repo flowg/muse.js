5d75c4bdeff38637bee08c8bbff8490cee92a4ba
Florian Gesta <flo.gesta@gmail.com>
2021-06-24 15:58:30 +0200

    Bumped the Angular's version that will be used in a generated nativescript app

--- packages/nativescript/src/generators/application/dependencies.ts
+++ packages/nativescript/src/generators/application/dependencies.ts
@@ -9,14 +9,14 @@ import { NativeScriptAppType } from './schema';
 export const DEPENDENCIES: Map<NativeScriptAppType, Record<string, string>> = new Map<NativeScriptAppType, Record<string, string>>(
     [
         [NativeScriptAppType.ANGULAR, {
-            '@angular/animations': '^11.0.0',
-            '@angular/common': '^11.0.0',
-            '@angular/compiler': '^11.0.0',
-            '@angular/core': '^11.0.0',
-            '@angular/forms': '^11.0.0',
-            '@angular/platform-browser': '^11.0.0',
-            '@angular/platform-browser-dynamic': '^11.0.0',
-            '@angular/router': '^11.0.0',
+            '@angular/animations': '^12.0.0',
+            '@angular/common': '^12.0.0',
+            '@angular/compiler': '^12.0.0',
+            '@angular/core': '^12.0.0',
+            '@angular/forms': '^12.0.0',
+            '@angular/platform-browser': '^12.0.0',
+            '@angular/platform-browser-dynamic': '^12.0.0',
+            '@angular/router': '^12.0.0',
             '@nativescript/angular': '~11.0.0',
             '@nativescript/core': '~7.1.0',
             '@nativescript/theme': '~3.0.0',
@@ -28,7 +28,7 @@ export const DEPENDENCIES: Map<NativeScriptAppType, Record<string, string>> = ne
 export const DEV_DEPENDENCIES: Map<NativeScriptAppType, Record<string, string>> = new Map<NativeScriptAppType, Record<string, string>>(
     [
         [NativeScriptAppType.ANGULAR, {
-            '@angular/compiler-cli': '^11.0.0',
+            '@angular/compiler-cli': '^12.0.0',
             '@ngtools/webpack': '~11.0.0',
             '@nativescript/webpack': '~4.0.0'
         }]
