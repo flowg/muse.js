e013c5beb95516037462d3b0b1de988038b74f0e
Florian Gesta <flo.gesta@gmail.com>
2021-07-25 19:55:51 +0200

    We can now deploy an Angular app through the Serverless Website Component

--- packages/ng-sls/src/generators/application/generator.ts
+++ packages/ng-sls/src/generators/application/generator.ts
@@ -7,15 +7,11 @@ import * as path from 'path';
  * Nx imports
  */
 import {
-  addDependenciesToPackageJson,
-  formatFiles,
-  GeneratorCallback,
-  joinPathFragments,
-  NxJsonProjectConfiguration,
-  ProjectConfiguration,
-  Tree,
-  updateJson,
-  updateProjectConfiguration
+    formatFiles,
+    GeneratorCallback,
+    installPackagesTask,
+    TargetConfiguration,
+    Tree
 } from '@nrwl/devkit';
 import { applicationGenerator } from '@nrwl/angular/src/generators/application/application';
 
@@ -23,97 +19,65 @@ import { applicationGenerator } from '@nrwl/angular/src/generators/application/a
  * 3rd-party imports
  */
 import {
-  addFiles,
-  NormalizedSchema,
-  normalizeOptions
+    addFiles,
+    addTargets2ProjectConfiguration,
+    NormalizedSchema,
+    normalizeOptions
 } from '@muse.js/mneme';
 
 /**
  * Internal imports
  */
 import { NgSlsAppGeneratorSchema } from './schema';
-/*import {
-  DEPENDENCIES,
-  DEV_DEPENDENCIES
-} from './dependencies';*/
 
-function generateProjectConfiguration(normalizedOptions: NormalizedSchema<NgSlsAppGeneratorSchema>): ProjectConfiguration & NxJsonProjectConfiguration {
-  // TODO: Check targets
-  return {
-    root: normalizedOptions.projectRoot,
-    projectType: 'application',
-    sourceRoot: `${normalizedOptions.projectRoot}/src`,
-    targets: {
-      deploy: {
-        executor: '@nrwl/workspace:run-commands',
-        options: {
-          commands: [
-            'cp -RL ../../node_modules .',
-            'sls deploy',
-            'rm -R node_modules'
-          ],
-          cwd: normalizedOptions.projectRoot,
-          parallel: false
+function generateNewTargets(normalizedOptions: NormalizedSchema<NgSlsAppGeneratorSchema>): Record<string, TargetConfiguration> {
+    return {
+        deploy: {
+            executor: '@nrwl/workspace:run-commands',
+            options: {
+                commands: [
+                    'sls deploy'
+                ],
+                cwd: normalizedOptions.projectRoot,
+                parallel: false
+            }
+        },
+        remove: {
+            executor: '@nrwl/workspace:run-commands',
+            options: {
+                commands: [
+                    'sls remove'
+                ],
+                cwd: normalizedOptions.projectRoot,
+                parallel: false
+            }
         }
-      },
-      remove: {
-        executor: '@nrwl/workspace:run-commands',
-        options: {
-          commands: [
-            'sls remove'
-          ],
-          cwd: normalizedOptions.projectRoot,
-          parallel: false
-        }
-      }
-    },
-    tags: normalizedOptions.parsedTags
-  };
+    };
 }
 
-export default async function (host: Tree, options: NgSlsAppGeneratorSchema): Promise<void> {
-  const normalizedOptions: NormalizedSchema<NgSlsAppGeneratorSchema> = normalizeOptions(host, options);
+export default async function (host: Tree, options: NgSlsAppGeneratorSchema): Promise<GeneratorCallback> {
+    const normalizedOptions: NormalizedSchema<NgSlsAppGeneratorSchema> = normalizeOptions(host, options);
 
-  // Generating a basic Angular app
-  await applicationGenerator(host, normalizedOptions);
+    // Generating a basic Angular app
+    await applicationGenerator(host, normalizedOptions);
 
-  // Adding stuff like targets and executors...
-  updateProjectConfiguration(host, normalizedOptions.projectName, generateProjectConfiguration(normalizedOptions));
+    // Adding new targets to the generated Angular project
+    addTargets2ProjectConfiguration<NgSlsAppGeneratorSchema>(
+        host, normalizedOptions, generateNewTargets(normalizedOptions)
+    );
 
-  /*
-   * Adding dependencies to the workspace's package.json so that
-   * the newly created Angular + Serverless application can build properly
-   * TODO: Adapt dependencies
-   */
-  /*const installTask: GeneratorCallback = addDependenciesToPackageJson(
-      host,
-      DEPENDENCIES,
-      DEV_DEPENDENCIES
-  );
+    // Generating files from templates
+    addFiles(host, normalizedOptions, path.join(__dirname, 'templates'));
 
-  /!*
-   * Improving default TypeScript configuration to prevent
-   * any warning during the compilation of lambda.ts
-   * TODO: Check if it's still necessary
-   *!/
-  updateJson(
-      host,
-      joinPathFragments(normalizedOptions.projectRoot, 'tsconfig.json'),
-      (json) => {
-        return {
-          ...json,
-          compilerOptions: {
-            esModuleInterop: true
-          }
-        };
-      }
-  );*/
+    // Formatting files according to Prettier
+    await formatFiles(host);
 
-  // Generating files from templates
-  addFiles(host, normalizedOptions, path.join(__dirname, 'templates'));
-
-  // Formatting files according to Prettier
-  await formatFiles(host);
-
-  // return installTask;
+    return () => {
+        /*
+         * We need to do this in a callback to install Node modules
+         * only when all changes to the host are done and the future file system
+         * is stable
+         */
+        installPackagesTask(host);
+    };
 }
