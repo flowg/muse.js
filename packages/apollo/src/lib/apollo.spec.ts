d8e07dba637e3880bdfea1e9096f519f3ff3ce3f
Florian Gesta <flo.gesta@gmail.com>
2021-08-17 16:22:19 +0200

    Deleted apollo package as a lib

--- packages/apollo/src/lib/apollo.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apollo } from './apollo';
-
-describe('apollo', () => {
-  it('should work', () => {
-    expect(apollo()).toEqual('apollo');
-  });
-});
--- packages/apollo/src/lib/apollo.ts
+++
@@ -1,3 +0,0 @@
-export function apollo(): string {
-  return 'apollo';
-}
